b5ef4c4dcff30a60b0775124f02b3769e9cf478f
sillysavant <long16012001.na@gmail.com>
2024-09-30 15:38:33 -0400

    First commit

---
+++ src/test/java/com/training/nf/NfFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.nf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NfFullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
